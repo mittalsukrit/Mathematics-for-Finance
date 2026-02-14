8584676a57e231144f23982410bdb820b57fd254
Sukrit Mittal <sukrit.mittal@franklintempleton.com>
2026-02-14 13:49:57 +0530

    updating the theme

--- lectures/convert.ps1
+++ lectures/convert.ps1
@@ -1,5 +1,5 @@
 Get-ChildItem -Filter "*.md" | ForEach-Object {
     $mdfile = $_.Name
     $htmlfile = $_.BaseName + ".html"
-    pandoc $mdfile -f gfm -s --mathjax -c https://cdnjs.cloudflar.com/ajax/libs/github-markdown-css/5.5.1/github-markdown.min.css -c wide.css -o $htmlfile
+    pandoc $mdfile -f markdown+raw_html -s --mathjax -c lecture-theme.css --metadata title="$($_.BaseName)" -o $htmlfile
 }
